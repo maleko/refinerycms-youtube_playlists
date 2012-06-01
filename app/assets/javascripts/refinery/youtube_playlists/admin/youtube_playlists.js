b50da5f657d15c62a38f65c8138f90d521c4efdc
Marc Lee <marc@adviso.com.au>
2012-06-01 10:18:57 +1000

    removing cargoculting cut and paste error

--- app/assets/javascripts/refinery/youtube_playlists/admin/youtube_playlists.js
+++ app/assets/javascripts/refinery/youtube_playlists/admin/youtube_playlists.js
@@ -11,25 +11,4 @@ $(function() {
       console.warn("error '" + textStatus + "' with '" + errorThrown);
     });
   });
-
-  $('ul.collapsible_menu').each(function(i, ul) {
-    (first_li = $(this).children('li:first')).after(div=$("<div></div>"));
-
-    $("<span class='arrow'>&nbsp;</span>").appendTo(first_li);
-
-    if (($(this).children('li.selected')).length === 0) {
-      div.hide();
-      first_li.addClass("closed");
-    }
-    $(this).children('li:not(:first)').appendTo(div);
-
-    first_li.find('> a, > span.arrow').click(function(e){
-      e.preventDefault();
-      $(this).parent().toggleClass("closed");
-      $(this).parent().toggleClass("open");
-
-      $(this).parent().next('div').animate({ opacity: 'toggle',height: 'toggle' }, 250, $.proxy(
-        (function() { $(this).css('background-image', null); }), $(this)));
-    });
-  });
 });
